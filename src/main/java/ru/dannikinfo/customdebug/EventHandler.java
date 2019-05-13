8755404211427e5039b489331378b4d96c917b14
dannikinfo <danil74808@gmail.com>
2019-05-13 19:18:36 +0700

    upload 1.11

---
+++ src/main/java/ru/dannikinfo/customdebug/EventHandler.java
@@ -0,0 +1,69 @@
+package ru.dannikinfo.customdebug;
+
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class EventHandler{
+	
+   final Minecraft mc = Minecraft.getMinecraft();
+
+   @SubscribeEvent(priority = EventPriority.HIGH)
+   public void onDrawOverlay(final RenderGameOverlayEvent e)
+   {
+	    if (e instanceof RenderGameOverlayEvent.Pre) {
+	        switch (e.getType()) {
+	            case DEBUG: {drawDebug(e);break;}
+	            default: {return;}
+	        }
+	        e.setCanceled(true);
+	    }
+	    
+   }
+   
+   public void drawDebug(RenderGameOverlayEvent e){
+	   
+	   FontRenderer fontrenderer = this.mc.fontRendererObj;
+	   ScaledResolution scaledresolution = new ScaledResolution(this.mc);
+	   
+	   int k = scaledresolution.getScaledWidth();
+       int l = scaledresolution.getScaledHeight();
+       int j3 = MathHelper.floor(this.mc.player.posX);
+       int k3 = MathHelper.floor(this.mc.player.posY);
+       int l3 = MathHelper.floor(this.mc.player.posZ);
+       
+	   BlockPos blockpos = new BlockPos(j3, k3, l3);
+       
+	   Chunk chunk = this.mc.world.getChunkFromBlockCoords(blockpos);
+       
+       long i5 = Runtime.getRuntime().maxMemory();
+       long j5 = Runtime.getRuntime().totalMemory();
+       long k5 = Runtime.getRuntime().freeMemory();
+       long l5 = j5 - k5;
+              
+       String usemem = I18n.format("customdebug.usemem") + ": " + l5 * 100L / i5 + "% - " + l5 / 1024L / 1024L + "MB / " + i5 / 1024L / 1024L + "MB";
+       String alocmem = I18n.format("customdebug.allocmem") + ": " + j5 * 100L / i5 + "% - " + j5 / 1024L / 1024L + "MB / " + i5 / 1024L / 1024L + "MB";
+       
+       GL11.glPushMatrix();
+       fontrenderer.drawStringWithShadow("Minecraft FPS: " + mc.getDebugFPS(), 2, 2, 16777215);
+       fontrenderer.drawStringWithShadow("x: " + (int) this.mc.player.posX , 2, 12, 16777215);
+       fontrenderer.drawStringWithShadow("y: " + (int) this.mc.player.posY , 2, 22, 16777215);
+       fontrenderer.drawStringWithShadow("z: " + (int) this.mc.player.posZ , 2, 32, 16777215);
+       fontrenderer.drawStringWithShadow(I18n.format("customdebug.biome") + ": " + chunk.getBiome(blockpos, this.mc.world.getBiomeProvider()).getBiomeName(), 2, 42, 16777215);
+       fontrenderer.drawStringWithShadow(I18n.format("customdebug.light") + ": " + chunk.getLightFor(EnumSkyBlock.BLOCK, blockpos), 2, 52, 16777215);
+       fontrenderer.drawStringWithShadow(usemem, k - fontrenderer.getStringWidth(usemem), 2, 16777215);
+       fontrenderer.drawStringWithShadow(alocmem, k - fontrenderer.getStringWidth(alocmem), 12, 16777215);
+       GL11.glPopMatrix();
+
+   }
+}
--- src/main/java/ru/dannikinfo/customdebug/Main.java
+++ src/main/java/ru/dannikinfo/customdebug/Main.java
@@ -1,33 +1,32 @@
-package su.hota.dannikinfo.customdebug;
+package ru.dannikinfo.customdebug;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod (modid = "customdebug", name = "Custom debug", version = "1.0")
+@Mod (modid = "customdebug", name = "Custom debug", version = "1.2")
 
 public class Main {
 
-	@SidedProxy(clientSide = "su.hota.dannikinfo.customdebug.ClientProxy") 	//proxy registering
+	@SidedProxy(clientSide = "ru.dannikinfo.customdebug.ClientProxy") 	//proxy registering
 	public static ClientProxy proxy;	//initialization proxy variable
 	@Instance("customdebug")		//instance initialization
 	public static Main instance;		//or here initialization instance :)
 	public static final String MODID = "customdebug";	//initialization mod id
 	public static final Logger logger = LogManager.getLogger(MODID);	//initialization logger 
 	
-	@EventHandler
+	@Mod.EventHandler
 	public void preLoad(FMLPreInitializationEvent event){
 		proxy.preInit(); 	//get preInit method of proxy
 	}
 	
-	@EventHandler
+	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event){
 	}
 	
@@ -36,7 +35,7 @@ public class Main {
 		proxy.init(); 	//get init method of proxy
 	}
 	
-	@EventHandler
+	@Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event){
 
     }
